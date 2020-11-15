f800ee3ccb4cbdd7dd30cf0748d52f077c4fd3ac
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2020-11-15 07:07:17 +0000

    update Video

--- flyer.js
+++ flyer.js
@@ -1,29 +1,50 @@
-$('head').append('<link rel="stylesheet" href="flyer.css" type="text/css" />');// AD CSS
+$('head').append('<link rel="stylesheet" href="/src/flyer/flyer.css" type="text/css" />');// AD CSS
+$('head').append('<link rel="stylesheet" href="/src/flyer/video/rtop.videoPlayer.1.0.2.min.css" />');
+$('head').append('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">');
+$('head').append('<script type="text/javascript" src="/src/flyer/video/rtop.videoPlayer.1.0.2.js"></script>');
+$(document).ready(function () {
+    var vid = $('#flyer_video').RTOP_VideoPlayer({
+        showFullScreen: true,
+        showTimer: true,
+        showSoundControl: true,
+        playInModal: false,
+    });
+});
+// put anything above === HERE === that needs to be loaded before hand. Such as CSS or scripts.
+console.log('flyer init loaded');
 // auto set ad
 $(document).ready(function() {
     //flyer css main
     // flyer
-    $("#flyer").append('<br><h2 class="flyer-title"><a href="#" id="flyer-linka"><i id="fyler-Sponsor-name"></i></a></h2> <div class="entry"> <desc><img class="flyer-image" id="flyer-image" src="/src/home/shared/images/HER.PNG" alt="" title="this is not the right image.." style="display: none;"/> <iframe id="flyer-frame" class="flyer-iframe9"></iframe> <p class="flyer-desc" id="flyer-desc"></p> </desc> </div>');
+    $("#flyer").append('<br><h2 class="flyer-title"><a href="#" id="flyer-linka"><i id="fyler-Sponsor-name"></i></a></h2><div class="entry"><desc><img class="flyer-image" id="flyer-image" src="/src/home/shared/images/HER.PNG" alt="" title="this is not the right image.." style="display:none;" /><iframe id="flyer-frame" class="flyer-iframe9"></iframe><div class="myVideo" id="flyer_video" data-video="/src/flyer/video/sample.mp4" data-poster="/src/flyer/video/sample.jpg" data-type="video/mp4"></div><p class="flyer-desc" id="flyer-desc"></p></desc></div>');
+    console.log('flyer markup Loaded');
     //Sponer name
-    $("#fyler-Sponsor-name").append('Sponsor: Spot Open!');
-    $("#flyer-linka").attr('a', '#');
-    $("#flyer-linka").attr('target', '_blank');
-    $("#flyer-linka").attr('alt', 'Link to Sponsor')
+    $("#fyler-Sponsor-name").append('A Word From Our Sponsor:'); // top name
+    $("#flyer-linka").attr('a', '#'); // href ro sponsor
+    $("#flyer-linka").attr('target', '#'); // target _blank for best.
+    $("#flyer-linka").attr('alt', 'Link to Sponsor') // alt to href
     // Sponsor Iframe
-    $("#flyer-frame").attr('src', 'flyer.htm');
-    $("#flyer-frame").attr('sandbox', '');
-    $("#flyer-frame").attr('allowpaymentrequest', 'false');
-    $("#flyer-frame").attr('name', 'Flyer');
-    $("#flyer-frame").attr('allowfullscreen', 'false');
-    $("#flyer-frame").attr('referrerpolicy', 'unsafe-url');
+    $("#flyer-frame").attr('src', '/settings/flyer.htm'); // link to html, htm or img.
+    $("#flyer-frame").attr('sandbox', ''); // edit if needed, to disable iframe scripts.
+    $("#flyer-frame").attr('allowpaymentrequest', 'false'); // do not allow payments
+    $("#flyer-frame").attr('name', 'Flyer'); // name of iframe ALT
+    $("#flyer-frame").attr('allowfullscreen', 'false');  // do not change.
+    $("#flyer-frame").attr('referrerpolicy', 'unsafe-url'); // what kind of URL is it? Be unsafe if you dont know.
     // Sponsor Dsec
-    $("#flyer-desc").append('Your ad here');
-    $("#flyer-desc").attr('alt', 'ad desc.');
+    $("#flyer-desc").append('Your ad here'); // Bottom text
+    $("#flyer-desc").attr('alt', 'ad desc.'); // bottom alt text
     // Sponsor image
-    $("#flyer-image").attr('src', '');
-    $("#flyer-image").attr('title', '');
-    $("#flyer-image").attr('alt', 'none');
+    $("#flyer-image").attr('src',  ''); // img SRC Gif, webm, PNG or jpeg.
+    $("#flyer-image").attr('title', ''); // title of img, alt
+    $("#flyer-image").attr('alt', 'none'); // alt of an alt
     $("#flyer-image").hide();
+    // Sponsor video
+    $("#flyer_video").attr('data-video','#'); //mp4 only
+    $("#flyer_video").attr('data-poster', '#'); // poster "tumbnail/cover"
+    $("#flyer_video").attr('data-type', 'video/mp4'); // data type
+    $("#flyer-image").hide();
+    //conf
+    console.log('flyer conf loaded');
 });
 // ad setting ON/OFF
 $(document).ready(function($) {
@@ -33,12 +54,19 @@ $(document).ready(function($) {
 
     $("#nofly").click(function() {
         $("#flyer").addClass("hide");
+        $("#yesfly").addClass("hide");
+        $("#nofly").addClass("hide");
         localStorage.setItem('ad', 'hide');
     });
 
     $("#yesfly").click(function() {
         $("#flyer").removeClass("hide");
+        $("#yesfly").removeClass("hide")
+        $("#nofly").removeClass("hide");
         localStorage.setItem('ad', null);
     });
 });
+console.log('flyer enable/disable conf loaded')
 // <div id="flyer"></div>
+// video stuff RTOP_VideoPlayer in LICENSE /src/flyer/video/LICENSE
+console.log('flyer fully loaded');
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Welcome to Flyer
+##### _what is flyer? flyer is a Simple ad script that helps small websites have a Single ad. and allow the use to have full control over it._
+# How to use
+## Add to head
+```html
+<script src="/link/to/flyer.js">
+<!-- include jquery or jq.js ...>
+```
+## Add to part where you want flyer
+```html
+<div id="flyer"></div>
+```
+# Flyer will take care of the rest
+
+## UI controls:
+```html
+    <button id="yesfly">Yes ads</button>
+    <button id="nofly">No ads</button>
+    <!-- not needed to run. -->
+```
+<br>
+<br>
+# Updates:
+```code
++ Video support (with the help of rtopvideo - MIT)
++ Video Conf
++ Console.log to find errors
++ flyer-min.js 
+/ broken Blanks
+/ CSS not loading due to stlye sheet not being linked right
+~ changed loading of some parts
+```
+
+```code
+update key:
++ Added
+/ Fixed
+~ changed
+- removed
+```
