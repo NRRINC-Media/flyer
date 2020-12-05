bac050ae5002de411f26c59dac3bf96c72d7736b
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2020-12-05 01:13:15 +0000

    flyerconf

---
+++ tests/1/flyer-conf.js
@@ -0,0 +1,9 @@
+localStorage.setItem("flyer-Sponsor-name", "A Word From Our Sponsor: Crispy"); // Sponser name
+localStorage.setItem("flyer-link-href", "#"); // Sponser link
+localStorage.setItem("flyer-link-href-alt","alt text"); // Sponser alt text, for screen readers
+localStorage.setItem("flyer-iframe-m-src","flyer.htm"); // flyer iframe link (mobile)
+localStorage.setItem("flyer-iframe-m-rp","unsafe-url"); // set unsafe URL, it is not recommended to change. unless loading from dif-domain
+localStorage.setItem("flyer-desc", "Your ad desc here"); // your ad desc.... duh.
+localStorage.setItem("flyer-desc-alt", "Your ad alt for screen readers"); // long desc, DUH,
+localStorage.setItem("flyer-video-data", "#"); // video data MP4 Only.
+localStorage.setItem("flyer-video-poster", '#'); // Cover video like thumbnail.
