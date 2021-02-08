96f0789e689b201ec17b9cd8b9d2e8d46d2a7f29
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2021-02-08 07:10:27 +0000

    Jquery Auto-added now.
    (Trying to move from jquery, but cmon' its jquery!)

--- src/flyer/flyer.js
+++ src/flyer/flyer.js
@@ -1,4 +1,11 @@
 // the Flyer Project, NRRINC Media (C) 2021, Apache V2 
+(function(){;var script = document.createElement("SCRIPT");script.src = '/src/flyer/jquery-embed.js';script.type = 'text/javascript';document.getElementsByTagName("head")[0].appendChild(script);
+var checkReady= function(callback){
+    if(window.jQuery){callback(jQuery);
+    }else{window.setTimeout(function() {
+         checkReady(callback);},20);
+        }};
+checkReady(function($){
 flyerinit();
 $(document).ready( function () {
     if ($("#flyer").length) {
@@ -326,3 +333,4 @@ function flyconsole() {
         return Function.prototype.bind.call(console.error, console, context, 'color:red;border:1px solid black');
     }();
 }
+});})();
