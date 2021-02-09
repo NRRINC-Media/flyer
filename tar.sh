f704d0f3af5ddcdf199647476efae1b83b0129a5
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2021-02-09 04:45:56 +0000

    .

--- tar.sh
+++ tar.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-ver="v1.3.1" #ex: v1.0.0 NO SPACES.
-tar -zcvf flyer-$ver.tar.gz flyer demo.html
+rm *flyer*.*tar.gz*
+rm *flyer**-sha256*
+echo "FlyerV: (ex: 1.3.1)"
+read ver
+tar -zcvf flyer-v$ver.tar.gz flyer demo.html
 sha256sum flyer-$ver.tar.gz > flyer-$ver-sha256
 echo "done"
